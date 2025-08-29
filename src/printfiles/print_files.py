50021237e0b03acb7b67263f073abd403d3029dd
Gilad Barnea <giladbrn@gmail.com>
2025-08-29 15:50:53 +0300

    reuse cli common in print files

--- src/printfiles/print_files.py
+++ src/printfiles/print_files.py
@@ -494,133 +494,21 @@ def resolve_extensions(
 
 
 def main():
-    epilog = textwrap.dedent(f"""
-    DEFAULT MATCH CRITERIA
-    When -t,--type is unspecified, the following file extensions are matched: {', '.join(resolve_extensions(custom_extensions=[], no_docs=False))}.
-
-    NOTE ABOUT EXCLUSIONS
-    Exclusions match rather eagerly, because each specified exclusion is handled like a substring match. For example, 'o/b' matches 'foo/bar/baz'.
-    Extension exclusions are stricter, so '.py' matches 'foo.py' but not 'foo.pyc'.
-    For more control, use glob patterns; specifying '*o/b' will match 'foo/b' but not 'foo/bar/baz'.
-    
-    KNOWN ISSUES
-    - There is no easy way to include .dotfiles without specifying --no-exclude.
-    """)
-    parser = argparse.ArgumentParser(
-        description="Prints the contents of files in a directory or specific file paths",
-        add_help=True,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=epilog,
-    )
-
-    parser.add_argument(
-        "paths",
-        type=str,
-        nargs="*",
-        help="Path(s) to one or more directories or files. Defaults to current directory if none specified.",
-        default=["."],
-    )
-
-    # For UI consistency, uppercase flags are boolean "include" flags.
-    parser.add_argument(
-        "-T",
-        "--include-tests",
-        action="store_true",
-        help="Include `test` and `tests` directories and spec.ts files.",
-    )
-    parser.add_argument(
-        "-L",
-        "--include-lock",
-        action="store_true",
-        help="Include lock files (e.g. package-lock.json, poetry.lock, Cargo.lock).",
-    )
-    parser.add_argument(
-        "-a",
-        "--text",
-        "--include-binary",
-        "--binary",
-        action="store_true",
-        dest="include_binary",
-        help="Include binary files (e.g. *.pyc, *.jpg, *.zip, *.pdf).",
-    )
-    parser.add_argument(
-        "-d",
-        "--no-docs",
-        action="store_true",
-        help="Exclude `.md`, `.mdx` and `.rst` files. Has no effect if -t,--type is specified.",
-    )
-    parser.add_argument(
-        "-E",
-        "--include-empty",
-        action="store_true",
-        help="Include empty files and files that only contain imports and __all__=... expressions.",
-    )
-    parser.add_argument(
-        "-l", "--only-headers", action="store_true", help="Print only the file paths."
-    )
-    parser.add_argument(
-        "-t",
-        "--type",
-        type=str,
-        default=[],
-        help="Match only files with the given extensions (e.g. 'py', '.py') or glob patterns (e.g. '*py*', '*.pyc?'). Can be specified multiple times.",
-        action="append",
-    )
-    parser.add_argument(
-        "-x",
-        "--exclude",
-        type=str,
-        help="Exclude files or directories whose path contains the given name/path or matches the given glob. Can be specified multiple times. By default, excludes "
-        + ", ".join(map(_describe_predicate, DEFAULT_EXCLUSIONS))
-        + ", and any files in .gitignore, .git/info/exclude, and ~/.config/git/ignore.",
-        default=[],
-        action="append",
-    )
-    parser.add_argument(
-        "--no-exclude",
-        action="store_true",
-        help="Disable all exclusions (overrides --exclude).",
-    )
-    parser.add_argument(
-        "-I",
-        "--no-ignore",
-        action="store_true",
-        help="Disable gitignore file processing. By default, exclusions are loaded from .gitignore, .git/info/exclude, and ~/.config/git/ignore.",
-    )
-    
-    parser.add_argument(
-        "--tag",
-        type=str,
-        choices=["xml", "md"],
-        default="xml",
-        help="Output format tag. 'xml' uses <path/to/file.ext> tags, 'md' uses markdown-style headers. Defaults to 'xml'.",
-    )
-
-    args = parser.parse_args()
-
-    extensions = resolve_extensions(custom_extensions=args.type, no_docs=args.no_docs)
-
-    exclusions = resolve_exclusions(
-        no_exclude=args.no_exclude,
-        custom_excludes=args.exclude,
-        include_tests=args.include_tests,
-        include_lock=args.include_lock,
-        include_binary=args.include_binary,
-        no_ignore=args.no_ignore,
-        paths=args.paths,
-    )
-
-    # Switch to shared engine to honor the same traversal/printing semantics
+    # Reuse shared CLI to avoid duplication
+    from .cli_common import parse_common_args, derive_filters_and_print_flags
     from .adapters.filesystem import FileSystemSource
     from .core import DepthFirstPrinter, StdoutWriter
     from .formatters import MarkdownFormatter, XmlFormatter
+
+    parser, args = parse_common_args()
+    extensions, exclusions, include_empty, only_headers = derive_filters_and_print_flags(args)
 
     formatter = XmlFormatter() if args.tag == "xml" else MarkdownFormatter()
     printer = DepthFirstPrinter(
         FileSystemSource(),
         formatter,
-        include_empty=args.include_empty,
-        only_headers=args.only_headers,
+        include_empty=include_empty,
+        only_headers=only_headers,
         extensions=extensions,
         exclude=exclusions,
     )
