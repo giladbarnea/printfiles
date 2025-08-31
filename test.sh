b68d8ea424dcf4e9e712edbdca023dc711665445
Gilad Barnea <giladbrn@gmail.com>
2025-08-31 10:45:03 +0300

    add ruff and dev shell scripts

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ -t 1 && -t 0 && "$USER" = giladbarnea && "$LOGNAME" = giladbarnea && "$CURSOR_AGENT" != 1 ]]; then
+    uv run python -m pytest tests --color=yes --code-highlight=yes "$@"
+else
+    uv run python -m pytest tests --color=no --code-highlight=no -vv "$@"
+fi
